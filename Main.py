b3dd3a6e8bbb2abac4f69beed5e258f9914feba5
Tenpart <70036882+Tenpart@users.noreply.github.com>
2020-08-27 21:51:49 +0300

    Add icon, and title

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 from tkinter import *
-from tkinter import tk
 
 
 root = Tk()
 
+#Config window
 root.geometry('800x600')
+root.title('Launcher HNP')
+root.iconbitmap('C:/Users/lesch/Documents/Photo/space_car_icon_149810.ico')
 root.resizable(width = False, height = False)
 
 
